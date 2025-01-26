2c0ea2829705b9d611d2dd76b2fe758b7714dfc0
Inspiro <genxroleplay25@gmail.com>
2025-01-26 22:42:23 +0530

    Update database.php

--- connection/database.php
+++ connection/database.php
@@ -1,8 +1,8 @@
 <?php 
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'technicalpashupati'; 
+$server = 'mysql-mariadb-sea01-10-101.zap-hosting.com';
+$username = 'zap1140889-6';
+$password = 'jvoug7xJFlElrLIc';
+$database = 'zap1140889-6'; 
 
 $connection = mysqli_connect($server, $username, $password, $database);
 $connectionobj = new mysqli($server, $username, $password, $database);
